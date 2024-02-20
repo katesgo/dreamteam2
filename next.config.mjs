ebdda21da3756654d8407b9e2c6a5ac777a23594
Kate <katesgo@gmail.com>
2024-02-20 12:00:51 -0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'liveblocks.io',
+				port: ''
+			}
+		]
+	},
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+	  ) => {
+		config.externals.push({ canvas: 'commonjs canvas' })
+		return config
+	  },
+};
 
 export default nextConfig;
