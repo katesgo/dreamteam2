c71262261388694e4a0c063d4d9e877ad2e8bf39
Kate <katesgo@gmail.com>
2024-02-21 12:28:39 -0500

    pls

--- next.config.mjs
+++ next.config.mjs
@@ -4,23 +4,18 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
-				protocol: 'https',
-				hostname: 'liveblocks.io',
-				port: ''
-			}
-		]
+				protocol: "https",
+				hostname: "liveblocks.io",
+				port: "",
+			},
+		],
 	},
 	webpack: (
 		config,
 		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
 	) => {
-		config.externals.push({
-			"utf-8-validate": "commonjs utf-8-validate",
-			"bufferutil": "commonjs bufferutil",
-			canvas: "commonjs canvas",			
-		});
-
-		return config;
+		config.externals.push({ canvas: "commonjs canvas" });
+    return config;	
 	},	
 	typescript: {
 		ignoreBuildErrors: true,
