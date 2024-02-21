7437237928efac124d5407c65ae78276f05dcb6a
Kate <katesgo@gmail.com>
2024-02-21 12:31:45 -0500

    exclude

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,21 @@
-import './src/env.mjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {		
+const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "liveblocks.io",
-				port: "",
-			},
-		],
+	  remotePatterns: [
+		{
+		  protocol: "https",
+		  hostname: "liveblocks.io",
+		  port: "",
+		},
+	  ],
 	},
 	webpack: (
-		config,
-		{ buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+	  config,
+	  { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
 	) => {
-		config.externals.push({ canvas: "commonjs canvas" });
-    return config;	
-	},	
-	typescript: {
-		ignoreBuildErrors: true,
-	},	
-};
-
-export default nextConfig;
+	  config.externals.push({ canvas: "commonjs canvas" });
+	  return config;
+	},
+  };
+  
+  export default nextConfig;
